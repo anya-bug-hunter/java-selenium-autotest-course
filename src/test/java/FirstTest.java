baf14d68b056a00adcefa32570c2f3cd4a47c42f
Anna <avermac@yandex.ru>
2025-12-31 17:10:32 +0300

    added test checkXpath

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -41,4 +41,22 @@ public class FirstTest extends BaseTest {
 
         Thread.sleep(1000);
     }
+
+    @Test
+    public void checkXpath() throws InterruptedException {
+        driver.get("https://limestore.com/ru_ru/product/28911_8969_454-temno_krasnyi");
+        Thread.sleep(8000);
+
+        WebElement title = driver.findElement(By.xpath("//div[@class='ProductTitlePrice__title']/h1"));
+        assertEquals("ПЛАТЬЕ МАКСИ ИЗ 100% ШЕРСТИ", title.getText());
+
+        List<WebElement> colors = driver.findElements(By.xpath("//div[@class='ColorSelector__items']/child::div"));
+        assertEquals(2, colors.size());
+
+        WebElement buttonCart = driver.findElement(By.xpath("//span[normalize-space(text())='Добавить в корзину']"));
+        assertTrue(buttonCart.isDisplayed());
+
+        WebElement fitBlock = driver.findElement(By.xpath("//div[contains(@class, 'text--gray') and position()=last()]"));
+        assertTrue(fitBlock.isDisplayed());
+    }
 }
